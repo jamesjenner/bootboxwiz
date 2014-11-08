bb70a4b4133a14f9d6d3592f9b496228b9fad7c2
James Jenner <james.g.jenner@gmail.com>
2014-11-08 17:30:27 +1000

    Initial version

--- README.md
+++ README.md
@@ -1,4 +1,50 @@
-bootboxwiz
+Bootbox Wizard
 ==========
+This extends Bootbox to provide a dialog that can use either static or dynamic steps through a wizard work flow.
 
-Bootbox dialog as a wizard dialog
+Please see http://bootboxjs.com for information about bootbox. Bootbox is a flexible dialog system leveraging bootstrap. 
+
+Quick Start
+----
+
+- [Download the latest release](https://github.com/jamesjenner/bootboxwizard/dist/bootboxwizard.min.js)
+- Clone the repository: `git clone https://github.com/jamesjenner/bootboxwizard.git`
+- Install with Bower: [Bower](http://bower.io): `bower install bootboxwiz`
+
+Example
+----
+Refer to test.html
+
+
+Running Tests
+----
+
+
+Building a minified release
+----
+
+
+License
+----
+The MIT License (MIT)
+
+Copyright (C) 2014 by James Jenner
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+
---
+++ bootboxwiz.js
@@ -0,0 +1,236 @@
+this['bootBoxWiz'] = {};
+    
+BootBoxWiz.TYPE_DYNAMIC = "dynamic";
+BootBoxWiz.TYPE_FIXED = "fixed";
+
+function BootBoxWiz(options) {
+  options = options || {};
+
+  this.onFinish = ((options.onFinish !== null && options.onFinish !== undefined) ? options.onFinish : function() {});
+
+  this.title = ((options.title !== null && options.title !== undefined) ? options.title : 'Wizard');
+
+  
+  this.stepBaseId = ((options.stepBaseId !== null && options.stepBaseId !== undefined) ? options.stepBaseId : 'wizard');
+  this.stepContent = ((options.stepContent !== null && options.stepContent !== undefined) ? options.stepContent : ['']);
+  
+  this.guideEnabled = ((options.guideEnabled !== null && options.guideEnabled !== undefined) ? options.guideEnabled : true);
+  
+  this.nbrSteps = this.stepContent.length;
+  this.stepLogic = [];
+  this.stepContentSize = [];
+  
+  this.currentStep = 1;
+}
+
+BootBoxWiz.prototype.launch = function() {
+  var messageContent = '';
+  var i;
+  var xFreq = 1000 / this.nbrSteps;
+  
+  if(this.guideEnabled) {
+    messageContent += 
+      '<div class="stepwizard">' +
+      '  <svg class="stepwizard-row" height="25px" viewBox="0 0 1000 50" style="width: inherit; height: inherit; padding-bottom:15px;">' +
+      '    <line x1="0" y1="25" x2="1000" y2="25" style="stroke:darkgray; stroke-width:1" />';
+    
+    for(i = 0; i < this.nbrSteps; i++) {
+      this.stepLogic[i] = function () { return 0};
+      this.stepContentSize[i] = 1;
+      messageContent += 
+        '    <circle id="' + this.stepBaseId + 'StepInd' + i + '" cx="' + ((xFreq / 2) + (xFreq * i)) + '" cy="25" r="20" stroke="darkgray" stroke-width="1" fill="white" />' +
+        '    <text id="' + this.stepBaseId + 'StepIndText' + i + '" text-anchor="middle" x="' + ((xFreq / 2) + (xFreq * i)) + '" y="25" style="font-size: 18; dominant-baseline: middle;" fill:"lightgray" >' + (i + 1) + '</text>';
+    }
+
+    messageContent += 
+      '  </svg>' +
+      '</div>';
+  }
+
+  messageContent += 
+    '<div class="stepwizard row">  ' +
+    '  <div class="col-md-12"> ' +
+    '    <form class="form-horizontal"> ';
+  
+  var currentId = 0;
+  for(i = 0; i < this.stepContent.length; i++) {
+    if(this.stepContent[i] instanceof Object) {
+      if(this.stepContent[i].content !== null && this.stepContent[i].content !== undefined) {
+        if(this.stepContent[i].content instanceof Array) {
+          // this.nbrSteps += this.stepContent[i].content.length - 1;
+          this.stepLogic[i] = ((this.stepContent[i].logic !== null && this.stepContent[i].logic) ? this.stepContent[i].logic : function () { return 0});
+          this.stepContentSize[i] = this.stepContent[i].content.length;
+          
+          for(var j = 0; j < this.stepContent[i].content.length; j++) {
+            messageContent += 
+              '      <div class="row ' + this.stepBaseId + 'wizardContent" id="' + this.stepBaseId + 'Content' + (currentId++) + '">' +
+              this.stepContent[i].content[j] +
+              '      </div>';
+          }
+        } else {
+          this.stepLogic[i] = function () { return 0};
+          this.stepContentSize[i] = 1;
+          messageContent += 
+            '      <div class="row ' + this.stepBaseId + 'wizardContent" id="' + this.stepBaseId + 'Content' + (currentId++) + '">' +
+            this.stepContent[i].content +
+            '      </div>';
+        }
+      }
+    } else {
+      messageContent += 
+        '      <div class="row ' + this.stepBaseId + 'wizardContent" id="' + this.stepBaseId + 'Content' + (currentId++) + '">' +
+        this.stepContent[i] +
+        '      </div>';
+    }
+  }
+  
+  messageContent += 
+    '    </form>' +
+    '  </div>' +
+    '</div>';
+    
+  this.dialog = bootbox.dialog({
+    title: this.title,
+    message: messageContent,
+    buttons: {
+      previous: {
+        label: "Previous",
+        // className: "pull-left btn-default",
+        className: "btn-default",
+        callback: this.previousStep.bind(this)
+      },
+      next: {
+        label: "Next",
+        className: "btn-primary",
+        // callback: function () {return this.nextStep()}.bind(this)
+        callback: this.nextStep.bind(this)
+      },
+      finish: {
+        label: "Finish",
+        className: "",
+        callback: this.onFinish
+      }
+    }
+  });  
+
+  // this.wizNavListItems = $('div.wizardNavPanel div a');
+  // this.wizContent = $('.' + this.stepBaseId + 'wizardContent');
+  this.currentWizPanel = 0;
+  this.currentWizPanelStep = 0;
+
+//    // listen for clicks on wizard nav buttons
+//    wizNavListItems.on('click', function (e) {
+//      e.preventDefault();
+//      var $target = $($(this).attr('href'));
+//      var $item = $(this);
+//
+//      if(!$item.hasClass('disabled')) {
+//        this.wizNavListItems.removeClass('btn-primary').addClass('btn-default');
+//        $item.addClass('btn-primary');
+//        this.wizContent.hide();
+//        $target.show();
+//        $target.find('input:eq(0)').focus();
+//      }
+//    }.bind(this));
+
+  this._hideAllSteps();
+  this._showWizPanel(this.currentWizPanel);
+
+  this.dialog.on("shown.bs.modal", function(dialog) {
+    $('#title').focus();
+    this._setPreviousDisabled(true);
+    this._setFinishDisabled(true);
+  }.bind(this));
+}
+
+BootBoxWiz.prototype.nextStep = function() {
+  this.currentWizPanelStep++;
+  this._hideWizPanel(this.currentWizPanel);
+  this._applyContentOffsets();
+  this._showWizPanel(this.currentWizPanel);
+
+  $('#title').focus();
+
+  this._setPreviousDisabled(false);
+
+  if(this.currentWizPanelStep === (this.nbrSteps - 1)) {
+    this._setNextDisabled(true);
+    this._setFinishDisabled(false);
+  }
+
+  return false;
+}
+
+BootBoxWiz.prototype.previousStep = function() {
+  this.currentWizPanelStep--;
+  this._hideWizPanel(this.currentWizPanel);
+  this._applyContentOffsets();
+  this._showWizPanel(this.currentWizPanel);
+
+  $('#title').focus();
+
+  this._setNextDisabled(false);
+  this._setFinishDisabled(true);
+
+  if(this.currentWizPanelStep === 0) {
+    this._setPreviousDisabled(true);
+  }
+
+  return false;
+}
+
+BootBoxWiz.prototype._hideAllSteps = function() {
+  $('.' + this.stepBaseId + 'wizardContent').hide();
+}
+
+BootBoxWiz.prototype._hideWizPanel = function(counter) {
+  $('#' + this.stepBaseId + 'Content' + counter).hide();
+  $('#' + this.stepBaseId + 'StepInd' + counter).attr("fill", "white");
+  $('#' + this.stepBaseId + 'StepInd' + counter).attr("stroke", "darkgray");
+  $('#' + this.stepBaseId + 'StepIndText' + counter).attr("fill", "lightgray");
+}
+
+BootBoxWiz.prototype._showWizPanel = function(counter) {
+  $('#' + this.stepBaseId + 'Content' + counter).show();
+  $('#' + this.stepBaseId + 'StepInd' + counter).attr("fill", "#428bca");
+  $('#' + this.stepBaseId + 'StepInd' + counter).attr("stroke", "#428bca");
+  $('#' + this.stepBaseId + 'StepIndText' + counter).attr("fill", "white");
+  $('#title').focus();
+}
+
+BootBoxWiz.prototype._applyContentOffsets = function(step) {
+  // Using https://github.com/macek/jquery-serialize-object to serialize to an object
+  var stepOffset = this.stepLogic[this.currentWizPanelStep]($('.stepwizard .form-horizontal').serializeObject());
+  
+  // iterate through previous steps and apply size
+  var previousStepsOffset = 0;
+  
+  // this.stepContentSize[i]
+  for(var i = 0; i < this.currentWizPanelStep; i++) {
+    previousStepsOffset += this.stepContentSize[i] - 1;
+  }
+  
+  this.currentWizPanel = previousStepsOffset + this.currentWizPanelStep + stepOffset
+  
+  console.log("stepOffset: " + stepOffset);
+  console.log("this.currentWizPanel: " + this.currentWizPanel);
+  
+  // return stepOffset;
+}
+
+
+BootBoxWiz.prototype._setPreviousDisabled = function(disable) {
+  this._getBootBoxButton('previous').prop('disabled', disable);
+}
+
+BootBoxWiz.prototype._setNextDisabled = function(disable) {
+  this._getBootBoxButton('next').prop('disabled', disable);
+}
+
+BootBoxWiz.prototype._setFinishDisabled = function(disable) {
+  this._getBootBoxButton('finish').prop('disabled', disable);
+}
+
+BootBoxWiz.prototype._getBootBoxButton = function(label) {
+  return $(".bootbox").find('button[data-bb-handler="' + label + '"]');
+}
